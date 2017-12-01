e8972051e33791bba1755d8d4aac4a49a986704c
michkjns <michkjns@gmail.com>
2017-12-01 11:58:34 +0100

    Added basic follow-camera script

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour 
+{
+	Transform followTargetTransform = null;
+	Vector3 offset;
+
+	void Start () 
+	{
+		followTargetTransform = GameManager.PlayerObject.transform;
+		Debug.Assert(followTargetTransform != null, "Camera has no target!");
+
+		offset = transform.position - followTargetTransform.position;
+	}
+	
+	void Update () 
+	{
+		//Vector3 position = transform.position;
+		//Vector3 lerpPosition = Vector3.Lerp(position, followTargetTransform, Time.deltaTime);
+	}
+
+	void LateUpdate()
+	{
+		transform.position = followTargetTransform.position + offset;
+	}
+}
