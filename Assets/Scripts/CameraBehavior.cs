ffc836f475b23ea1cf8b9d0c93dfe74f0065cd66
michkjns <michkjns@gmail.com>
2017-12-07 13:15:47 +0100

    Added new Camera behavior and custom editor

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour 
+{
+	private Transform followTargetTransform = null;
+	public enum CameraMode
+	{
+		Static,
+		Direct,
+		Horizontal
+	};
+
+	[SerializeField]
+	private CameraMode mode;
+	public CameraMode Mode { get { return mode; } }
+
+	// Direct
+	[SerializeField]
+	private Vector3 offset;
+	//
+
+	// Horizontal
+	[SerializeField]
+	private float maxDistance;
+
+	[SerializeField]
+	private float lerpSpeed;
+	//
+
+	void Start()
+	{
+		followTargetTransform = GameManager.PlayerObject.transform;
+		Debug.Assert(followTargetTransform != null, "Camera has no target!");
+
+		if (mode == CameraMode.Direct)
+		{
+			offset = transform.position - followTargetTransform.position;
+		}
+	}
+	
+	void Update () 
+	{
+		if (mode == CameraMode.Horizontal)
+		{
+			UpdateHorizontal();
+		}
+	}
+
+	void LateUpdate()
+	{
+		if (mode == CameraMode.Direct)
+		{
+			UpdateDirect();
+		}
+	}
+
+	void UpdateDirect()
+	{
+		transform.position = followTargetTransform.position + offset;
+	}
+
+	void UpdateHorizontal()
+	{
+		float horizontalDistance = followTargetTransform.position.x - transform.position.x;
+		float direction = horizontalDistance >= 0.0f ? 1.0f : -1.0f;
+		if (Mathf.Abs(horizontalDistance) > maxDistance)
+		{
+			transform.position = Vector3.Lerp(transform.position, 
+				new Vector3(transform.position.x + direction * (Mathf.Abs(horizontalDistance) - maxDistance), transform.position.y, transform.position.z), Time.deltaTime * lerpSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/CameraBehaviorEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraBehavior))]
+public class CameraBehaviorEditor : Editor
+{
+	public SerializedProperty cameraOffset;
+	public SerializedProperty horizontalMaxDistance;
+	public SerializedProperty horizontalLerpSpeed;
+	public SerializedProperty cameraMode;
+
+	void OnEnable()
+	{
+		// Setup the SerializedProperties
+		cameraOffset = serializedObject.FindProperty("offset");
+		horizontalMaxDistance = serializedObject.FindProperty("maxDistance");
+		horizontalLerpSpeed = serializedObject.FindProperty("lerpSpeed");
+
+		cameraMode = serializedObject.FindProperty("mode");
+	}
+
+	override public void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(cameraMode, new GUIContent("Mode"));
+
+		switch ((CameraBehavior.CameraMode)cameraMode.enumValueIndex)
+		{
+			case CameraBehavior.CameraMode.Direct:
+			{
+				EditorGUILayout.PropertyField(cameraOffset, new GUIContent("Offset"));
+				break;
+			};
+			case CameraBehavior.CameraMode.Horizontal:
+			{
+				EditorGUILayout.PropertyField(horizontalMaxDistance, new GUIContent("Max Distance"));
+				EditorGUILayout.PropertyField(horizontalLerpSpeed, new GUIContent("Lerp Speed"));
+				break;
+			}
+			case CameraBehavior.CameraMode.Static:
+			{
+				break;
+			}
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
